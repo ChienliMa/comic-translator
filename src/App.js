bab527708c2e5f55e607ec22cec4844193f37819
ChienliMa <maqianlie@gmail.com>
2017-07-02 00:05:57 +0800

    Finished reconstruction. New data model work.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,13 @@ var max = (a, b) => {return a > b ? a : b;};
 const VERTICALLY = 'vertivally';
 const HORIZENTALLY = 'horizentally';
 
-const DRAG_TO_COVER = "DRAG_TO_COVER";
+const DRAG_RECT = "DRAG_RECT";
 const DRAG_TEXT = "DRAG_TEXT";
+
+const KEYCODE_E = 69;
+const KEYCODE_F = 70;
+const KEYCODE_G = 71;
+const KEYCODE_SPACE = 32;
 
 function distance (a, b) {
     var sum = 0;
@@ -31,11 +36,10 @@ class Project {
 }
 
 class Page {
-    constructor (){
-        this.image = null;
+    constructor (src){
+        this.src = src;
         this.rects = []; // rects that cover original texts
         this.texts = [];
-        this.page = null;
     }
 }
 
@@ -48,7 +52,7 @@ class Text {
 
         // for dragging
         this.anchor = null;
-        this,oriPos = null;
+        this.oriPos = null;
 
         this.fontSize = 15;
         this.fontStyle = "serif";
@@ -150,71 +154,87 @@ class App extends Component {
 class InteractiveEditor extends Component {
     constructor(props) {
         super(props);
+        this.state = props;
 
         var page = new Page();
-        page.project = props.project;
-
         var demo_img = new Image();
         demo_img.src = testimg;
         page.image = demo_img;
 
-        // for interactive component
-        page.canvas_w = 400;
-        page.canvas_h = 400;
+        this.page = page;
 
+        // for interactive component
         this.drag = {};
         this.drag.state = "";
         this.drag.item = null;
         this.drag.startPos = null;
-
-        this.state = page;
-    }
-
-    setCanvasSize() {
-        var div = document.getElementById("EditorContainer");
-
-        this.state.canvas_w = min(this.state.image.width, div.clientWidth);
-        this.state.canvas_h = this.state.canvas_w * this.state.ratio;
-    }
-
-
-    updateCanvas() {
-        this.setCanvasSize();
-        var baseCanvas = document.getElementById("BaseCanvas");
-        baseCanvas.height = this.state.canvas_h;
-        baseCanvas.width = this.state.canvas_w;
-        var ctx = baseCanvas.getContext("2d");
-
-        ctx.drawImage(this.state.image, 0, 0, this.state.canvas_w, this.state.canvas_h);
-
-
-        var coverCanvas = document.getElementById("CoverCanvas");
-        coverCanvas.height = this.state.canvas_h;
-        coverCanvas.width = this.state.canvas_w;
-        var textCanvas = document.getElementById("TextCanvas");
-        textCanvas.height = this.state.canvas_h;
-        textCanvas.width = this.state.canvas_w;
+        this.keyPressed = {};
     }
 
     componentDidMount() {
-        this.state.project.proxy.subscribe("SelectImg", this.updateImage.bind(this));
+        this.state.project.proxy.subscribe("SelectImg", this.switchToPage.bind(this));
 
-        if (this.state.image.complete) {
+        this.ctxes = {};
+        this.ctxes.base = document.getElementById("BaseCanvas").getContext("2d");
+        this.ctxes.rect = document.getElementById("RectCanvas").getContext("2d");
+        this.ctxes.text = document.getElementById("TextCanvas").getContext("2d");
+
+        this.loadDemoImage();
+        this.registerListeners();
+    }
+
+    loadDemoImage() {
+        if (this.page.image.complete) {
             this.updateCanvas();
         } else {
-            this.state.image.onload = () => this.updateCanvas();
+            this.page.image.onload = () => {this.resizeCanvases(); this.refreshAllCanvases();};
         }
     }
 
-    updateImage(src) {
-        var state = this.state;
-        var image = new Image();
-        image.src = src;
-        image.crossOrigin = "Anonymous";
-        state.image = image;
-        state.ratio = image.height / image.width;
-        this.setState(state);
-        this.updateCanvas();
+    registerListeners () {
+        var model = this;
+        document.body.addEventListener('keydown', function(event) {
+            model.keyPressed[event.keyCode] = true;
+        });
+
+        document.body.addEventListener('keyup', function(event) {
+            model.keyPressed[event.keyCode] = false;
+        });
+    }
+
+    switchToPage (page) {
+        this.page = page;
+        this.resizeCanvases();
+        this.refreshAllCanvases();
+    }
+
+    resizeCanvases () {
+        for(var ctx in this.ctxes) {
+            this.ctxes[ctx].canvas.width = this.page.image.width;
+            this.ctxes[ctx].canvas.height = this.page.image.height;
+        }
+    }
+
+    refreshAllCanvases () {
+        this.refreshBaseCanvas();
+        this.refreshRectCanvas();
+        this.refreshTextCanvas();
+    }
+
+    refreshBaseCanvas () {
+        this.ctxes.base.drawImage(this.page.image, 0, 0);
+    }
+
+    refreshRectCanvas () {
+        var canvas = this.ctxes.rect.canvas;
+        this.ctxes.rect.clearRect(0,0,canvas.width,canvas.height);
+        this.page.texts.map((x) => {x.renderOnContext(this.ctxes.rect)});
+    }
+
+    refreshTextCanvas () {
+        var canvas = this.ctxes.text.canvas;
+        this.ctxes.rect.clearRect(0,0,canvas.width,canvas.height);
+        this.page.texts.map((x) => {x.renderOnContext(this.ctxes.text);});
     }
 
     getMouseRelativePosition(canvas, event) {
@@ -226,31 +246,51 @@ class InteractiveEditor extends Component {
         var ctx = canvas.getContext('2d');
     }
 
+    onDragStart(event) {
+        var canvas = document.getElementById("TextCanvas");
+        var pos = this.getMouseRelativePosition(canvas, event);
+
+        switch (this.keyPressed) {
+            case KEYCODE_F :
+                this.rectDragStart(pos);
+                break;
+            case KEYCODE_E :
+                this.textDragStart(pos);
+                break;
+            default:
+                break;
+        }
+    }
+
     rectDragStart (pos) {
         var newRect = new Rect(pos, pos.copy());
-        this.state.rects.push(newRect);
-        this.drag.state = DRAG_TO_COVER;
+        this.page.rects.push(newRect);
+        this.drag.state = DRAG_RECT;
         this.drag.item = newRect;
     }
 
     textDragStart (text, pos) {
-        this.drag.state = DRAG_TEXT;
-        this.drag.item = text;
-        text.setAnchor(pos);
+        for (var text of this.page.texts) {
+            if (text.mouseWithin(pos)) {
+                this.drag.state = DRAG_TEXT;
+                this.drag.item = text;
+                text.setAnchor(pos);
+                break;
+            }
+        }
     }
 
-    onDragStart(event) {
+    onDrag (event) {
         var canvas = document.getElementById("TextCanvas");
         var pos = this.getMouseRelativePosition(canvas, event);
-        if (event.shift) {  //drag to draw rect
-            this.rectDragStart(pos);
-        } else if (event.alt) {  //drag text
-            for (var text of this.state.texts) {
-                if (text.mouseWithin(pos)) {
-                    this.textDragStart(text, pos);
-                    break;
-                }
-            }
+        switch (this.drag.state){
+            case DRAG_RECT:
+                this.rectDragUpdate(pos);
+                break;
+            case DRAG_TEXT:
+                this.textDragUpdate(pos);
+                break;
+            default:
         }
     }
 
@@ -264,47 +304,19 @@ class InteractiveEditor extends Component {
         this.refreshTextCanvas();
     }
 
-    onDrag (event) {
-        var canvas = document.getElementById("TextCanvas");
-        var pos = this.getMouseRelativePosition(canvas, event);
-        switch (this.state.drag.state){
-            case DRAG_TO_COVER:
-                this.rectDragUpdate(pos);
-                break;
-            case DRAG_TEXT:
-                this.textDragUpdate(pos);
-                break;
-            default:
-        }
-    }
-
     onDragEnd (event) {
         if (this.drag.state == DRAG_TEXT) this.drag.item.clearAnchor();
         this.drag = {};
     }
 
-    refreshRectCanvas() {
-        var canvas = document.getElementById("RectCanvas");
-        var ctx = canvas.getContext("2d");
-        ctx.clear();
-        this.state.rects.map((x) => {x.renderOnContext(ctx);});
-    }
-
-    refreshTextCanvas() {
-        var canvas = document.getElementById("TextCanvas");
-        var ctx = canvas.getContext("2d");
-        ctx.clear();
-        this.state.texts.map((x) => {x.renderOnContext(ctx);});
-    }
-
     render() {
         return (
-            <div style={{position: "relative", overflow: "auto", width: "100%"}}>
+            <div style={{position: "relative", overflow: "auto", width: "100%"}} >
                 <canvas id="BaseCanvas" style={{maxWidth: "100%", position: "absolute", zIndex: 1}}></canvas>
-                <canvas id="CoverCanvas" style={{maxWidth: "100%", position: "absolute", zIndex: 2}}></canvas>
+                <canvas id="RectCanvas" style={{maxWidth: "100%", position: "absolute", zIndex: 2}}></canvas>
                 <canvas id="TextCanvas" draggable="true" style={{maxWidth: "100%", position: "absolute", zIndex: 3}}
-                        onDragEnd={this.onDragEnd} onDrag={this.onDrag} onDragStart={this.onDrag}
-                        onClick={this.onClick}></canvas>
+                        onDragEnd={this.onDragEnd.bind(this)} onDrag={this.onDrag.bind(this)}
+                        onDragStart={this.onDragStart.bind(this)}></canvas>
             </div>
         )
     }
@@ -324,7 +336,9 @@ class GalleryComponent extends React.Component {
             var page = new Page();
             page.image = new Image();
             page.image.src = event.target.result;
+
             page.key = this.state.pages.length;
+            page.image.crossOrigin = "Anonymous";
 
             this.state.pages.push(page);
             this.setState(this.state);
@@ -341,8 +355,8 @@ class GalleryComponent extends React.Component {
     renderThumbnails() {
         return (
             this.state.pages.map(
-                (x) => {
-                    return ( <ThumbnailComponent src={x.image.src} key={x.key} proxy={this.state.proxy}></ThumbnailComponent>)
+                (page) => {
+                    return ( <ThumbnailComponent page={page} key={page.key} proxy={this.state.proxy}></ThumbnailComponent>)
                 }
             )
         )
@@ -362,16 +376,18 @@ class GalleryComponent extends React.Component {
 class ThumbnailComponent extends Component {
     constructor (props) {
         super(props);
-        this.state = props;
+        this.page = props.page;
+        this.proxy = props.proxy;
     }
 
     onClick (event) {
-        this.state.proxy.trigger("SelectImg", this.state.src);
+        this.proxy.trigger("SelectImg", this.page);
     }
 
     render () {
         return(
-            <img onClick={this.onClick.bind(this)} alt={this.state.key} src={this.state.src} style={{maxWidth:"100%", display:"block",float:"left"}}></img>
+            <img src={this.page.image.src} onClick={this.onClick.bind(this)}
+                 style={{maxWidth:"100%", display:"block",float:"left"}}></img>
         )
     }
 }
